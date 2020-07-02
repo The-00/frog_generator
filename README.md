8cacaf351e74805e53291c25ef29a56308b657e4
theau <theau.trova@etu.univ-lyon1.fr>
2020-07-02 23:16:12 +0200

    V1 frog generator

---
+++ README.md
@@ -0,0 +1,3 @@
+# Frog generator
+
+
---
+++ generator.py
@@ -0,0 +1,34 @@
+from frog import Frog
+from PIL import Image
+from time import time
+
+X, Y = 30, 2
+frogsize = 500
+frogs = Image.new("RGBA", (frogsize*X,frogsize*Y), "#0000")
+lfrogs = []
+n = 50
+
+t1 = time()
+k = 0
+for y in range(Y):
+    for x in range(X):
+        k+=1
+        frog = Frog(goodeyes=True)
+
+        frogim = frog.get(frogsize)
+        lfrogs.append(frogim)
+        frogs.paste( frogim, (x*frogsize, y*frogsize))
+
+        percent = 100*k/(X*Y)
+        naffi = int(percent/(100/n))
+        print("[", "#"*naffi, " "*(n-naffi),"]", str(percent)+"%     ", end="\r")
+print("[", "#"*n,"]", "100%   ")
+
+t2 = time()
+print(round(t2-t1,3), "s pour", X*Y, "grenouilles de tailles",frogsize)
+print(round((t2-t1)/(X*Y),3), "s/grenouille de taille",frogsize)
+
+frogs.save("frog.png")
+lfrogs[0].save('frog.gif', save_all=True, append_images=lfrogs[1:], optimize=False, duration=120, loop=0)
+
+
